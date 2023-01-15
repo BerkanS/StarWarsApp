bb908145367e1f36d35a03d3507f34874c13a0f5
Berkan <serbest_berkan00@hotmail.com>
2023-01-16 00:55:24 +0100

    Finished PagingSource

---
+++ app/src/main/java/com/berkan/starwarsapp/data/PeoplePagingSource.kt
@@ -0,0 +1,37 @@
+package com.berkan.starwarsapp.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.berkan.starwarsapp.data.remote.StarWarsApi
+import com.berkan.starwarsapp.domain.model.Person
+
+private const val STARTING_PAGE_INDEX = 1
+
+class PeoplePagingSource(
+    private val starWarsApi: StarWarsApi
+) : PagingSource<Int, Person>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Person>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1) ?:
+            state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Person> {
+        val pageIndex = params.key ?: STARTING_PAGE_INDEX
+
+        return try {
+            val people = starWarsApi.getPeople(page = pageIndex).people
+            LoadResult.Page(
+                data = people,
+                prevKey = if (pageIndex == 1) null else pageIndex.minus(1),
+                nextKey = if (people.isEmpty()) null else pageIndex.plus(1)
+            )
+
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+}
--- app/src/main/java/com/berkan/starwarsapp/presentation/people_list/PeopleListScreen.kt
+++ app/src/main/java/com/berkan/starwarsapp/presentation/people_list/PeopleListScreen.kt
@@ -3,30 +3,31 @@ package com.berkan.starwarsapp.presentation.people_list
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
 
 @Composable
 fun PeopleListScreen(
     viewModel: PeopleListViewModel = hiltViewModel()
 ) {
+    val people = viewModel.getPeople().collectAsLazyPagingItems()
+
     Box {
-        viewModel.listOfPeople.let { list ->
-            LazyColumn(
-                content = {
-                    items(list) { person ->
-                        Surface(
-                            modifier = Modifier.padding(24.dp)
-                        ) {
-                            Text(text = person.name)
-                        }
+        LazyColumn { items(people) { person ->
+                person?.let {
+                    Surface(
+                        modifier = Modifier.padding(24.dp)
+                    ) {
+                        Text(text = person?.name ?: "empty")
                     }
-            })
+                }
+            }
         }
 
     }
