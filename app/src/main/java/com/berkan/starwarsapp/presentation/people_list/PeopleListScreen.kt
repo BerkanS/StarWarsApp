05cfde471ceb0d022adeab8c1d91eac2f96c1538
Berkan <serbest_berkan00@hotmail.com>
2023-01-16 01:39:05 +0100

    Added loading and error state

--- app/src/main/java/com/berkan/starwarsapp/presentation/people_list/PeopleListScreen.kt
+++ app/src/main/java/com/berkan/starwarsapp/presentation/people_list/PeopleListScreen.kt
@@ -1,16 +1,23 @@
 package com.berkan.starwarsapp.presentation.people_list
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+
+private const val DEFAULT_ERROR = "Something went wrong.."
 
 @Composable
 fun PeopleListScreen(
@@ -18,17 +25,37 @@ fun PeopleListScreen(
 ) {
     val people = viewModel.getPeople().collectAsLazyPagingItems()
 
-    Box {
-        LazyColumn { items(people) { person ->
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyColumn {
+            items(people) { person ->
                 person?.let {
                     Surface(
                         modifier = Modifier.padding(24.dp)
                     ) {
-                        Text(text = person?.name ?: "empty")
+                        Text(text = person.name)
                     }
+                    Divider()
                 }
             }
-        }
 
+            when (val state = people.loadState.refresh) {
+                is LoadState.Loading -> {
+                    item { LoadingItem()  }
+                }
+                is LoadState.Error -> {
+                    item { Text(text = state.error.message ?: DEFAULT_ERROR) }
+                }
+                else -> {}
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewList() {
+    PeopleListScreen()
 }
